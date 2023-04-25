3059e4776260d2abdc57de1d7b08bfd3d3b32e99
Kibet <kibetem24@gmail.com>
2023-04-25 23:28:10 +0300

    Modify top button

--- src/Components/Homepage.tsx
+++ src/Components/Homepage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Hover } from "../Layouts/Hover";
 import { Navbar } from "../Layouts/Navbar";
 import { Hero } from "../Layouts/Hero";
@@ -6,8 +6,10 @@ import { Slider } from "../Layouts/Slider";
 import { Filter } from "./Filter";
 import ProductDeals from "./ProductDeals";
 import { Offers } from "./Offers";
+import { BiArrowToTop } from "react-icons/bi";
 
 export const Homepage = () => {
+  const [showButton, setShowButton] = useState(false);
   const [navbar, setNavbar] = useState(false);
   const changeNavbar = () => {
     if (window.scrollY >= 110) {
@@ -16,6 +18,24 @@ export const Homepage = () => {
     } else {
       setNavbar(false);
     }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  // This function will scroll the window to the top
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // for smoothly scrolling
+    });
   };
   window.addEventListener("scroll", changeNavbar);
   return (
@@ -26,6 +46,16 @@ export const Homepage = () => {
       <Filter />
       <ProductDeals />
       <Offers />
+      {showButton && (
+        <div className="flex justify-end px-4 py-6">
+          <button
+            onClick={scrollToTop}
+            className="bg-blue-300 w-16 h-16 rounded-full border-yellow-500 flex justify-center items-center"
+          >
+            <BiArrowToTop className="h-10 w-10" />
+          </button>
+        </div>
+      )}
     </>
   );
 };
--- src/Layouts/Hover.tsx
+++ src/Layouts/Hover.tsx
@@ -3,8 +3,8 @@ import { BsHeart } from "react-icons/bs";
 
 export const Hover = () => {
   return (
-    <div className="relative transition ease-linear duration-1000">
-      <div className="bg-white flex items-center justify-between p-4 fixed right-0 left-0 z-50 shadow-sm h-24">
+    <div className="relative">
+      <div className="bg-white flex items-center justify-between p-4 fixed right-0 left-0 z-50 shadow-sm h-24 ease-in delay-1000">
         <div className="logo-img">
           <img src="src/assets/Images/logo-1.png" className="w-36" alt="Logo" />
         </div>
